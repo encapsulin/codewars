868a7d0ce746c1856c66aa64776860e360fcc39c
encapsulin <encapsulin@gmail.com>
2022-07-31 10:04:12 +0200

    initial commit

---
+++ Permutations.java
@@ -0,0 +1,34 @@
+package codewars;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Permutations {
+
+    static Set<String> set = new HashSet<>();
+    public static List<String> singlePermutations(String s) {
+        set = new HashSet<>();
+        System.out.println(s);
+        solve(s);
+        List<String> list = set.stream().collect(Collectors.toList());
+        return list;
+    }
+
+    public static void solve(String str_){
+        set.add(str_);
+
+        for(int i=0; i< str_.length()-1; i++){
+            byte[] str_array = str_.getBytes();
+
+            byte tmp = str_array[i];
+            str_array[i] = str_array[i+1];
+            str_array[i+1] = tmp;
+
+            String s = new String(str_array);
+            if(set.add(s))
+                solve(s);
+        }
+    }
+}
