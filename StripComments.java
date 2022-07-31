fde0447942277bc9a400f51a5b7c51ee36548bd5
encapsulin <encapsulin@gmail.com>
2022-07-31 10:51:11 +0200

    clean description

--- StripComments.java
+++ StripComments.java
@@ -1,5 +1,4 @@
 /*
-https://www.codewars.com/kata/51c8e37cee245da6b40000bd
 DESCRIPTION:
 Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
 
