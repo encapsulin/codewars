8f3c08c0462a2caabffc18232987d1e5f63b7b8b
encapsulin <encapsulin@gmail.com>
2022-07-31 10:49:35 +0200

    removed URL from description

--- Permutations.java
+++ Permutations.java
@@ -1,5 +1,4 @@
 /*
-https://www.codewars.com/kata/5254ca2719453dcc0b00027d/java
 DESCRIPTION:
 In this kata you have to create all permutations of a non empty input string and remove duplicates, if present. 
 This means, you have to shuffle all letters from the input in all possible orders.
