bf0588a18faffeaef6e3a7c1f3ed38f8bec9186a
encapsulin <encapsulin@gmail.com>
2022-07-31 10:15:05 +0200

    description added

--- Permutations.java
+++ Permutations.java
@@ -1,3 +1,10 @@
+/*
+https://www.codewars.com/kata/5254ca2719453dcc0b00027d/java
+DESCRIPTION:
+In this kata you have to create all permutations of a non empty input string and remove duplicates, if present. 
+This means, you have to shuffle all letters from the input in all possible orders.
+/*
+
 package codewars;
 
 import java.util.HashSet;
